ea4a62588e5a9435b3619d9dcdf803273b3fa083
meowleo <nkm.ko3e26@gmail.com>
2021-11-21 20:42:05 +0900

    Push

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSvkpGzpX0CK6PT6iFKAPjmHQGPKo5w4I",
+  authDomain: "cheesyhanpen.firebaseapp.com",
+  projectId: "cheesyhanpen",
+  storageBucket: "cheesyhanpen.appspot.com",
+  messagingSenderId: "830670424166",
+  appId: "1:830670424166:web:131de98406649b908b05a7",
+  measurementId: "G-V25T5BSNHM",
+}
+
+firebase.initializeApp(firebaseConfig)
